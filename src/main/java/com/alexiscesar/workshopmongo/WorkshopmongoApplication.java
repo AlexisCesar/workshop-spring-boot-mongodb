26d6a2a5e889b01d65c76e72ee5db68cfd42f4a4
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-06-06 22:49:20 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/alexiscesar/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.alexiscesar.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
